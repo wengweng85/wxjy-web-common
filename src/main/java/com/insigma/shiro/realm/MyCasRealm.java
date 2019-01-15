98e06f26574395e39b26b8a09ccf20ce7836ec93
wengweng85 <whengshaohui@163.com>
2019-01-15 12:09:40 +0800

    cas修改,可以获取自定义相关信息

--- src/main/java/com/insigma/shiro/realm/MyCasRealm.java
+++ src/main/java/com/insigma/shiro/realm/MyCasRealm.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
@@ -14,25 +15,33 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.cas.CasAuthenticationException;
 import org.apache.shiro.cas.CasRealm;
+import org.apache.shiro.cas.CasToken;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.CollectionUtils;
+import org.apache.shiro.util.StringUtils;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.validation.Assertion;
 import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
 import org.jasig.cas.client.validation.Saml11TicketValidator;
+import org.jasig.cas.client.validation.TicketValidationException;
 import org.jasig.cas.client.validation.TicketValidator;
 
 import com.insigma.common.util.SUserUtil;
 import com.insigma.common.util.StringUtil;
 import com.insigma.http.HttpRequestUtils;
 import com.insigma.mvc.UriConstraints;
-import com.insigma.mvc.model.AccessToken;
 import com.insigma.mvc.model.SPermission;
 import com.insigma.mvc.model.SRole;
 import com.insigma.mvc.model.SUser;
+import com.insigma.resolver.AppException;
 
 public class MyCasRealm extends CasRealm {
 
@@ -48,30 +57,54 @@ public class MyCasRealm extends CasRealm {
 	@Override
     public AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
 		//调用父类的方法，然后授权用户
-		AuthenticationInfo authc = super.doGetAuthenticationInfo(authcToken);
-		//获取用户名
-		String username = (String) authc.getPrincipals().getPrimaryPrincipal();
-		//根据用户名获取用户信息
-		try {
-				 HashMap map=new HashMap();
-	        	 map.put("username", username);
-				 AccessToken accessToken = (AccessToken) httpRequestUtils.httpPostObject(UriConstraints.API_TOKEN,map,AccessToken.class);
-				 SUser suser=new SUser();
-				 suser.setUsername(accessToken.getUsername()); 
-				 suser.setToken(accessToken.getToken());
-				 suser.setName(accessToken.getName());
-		
-				 map=new HashMap();
-				 map.put("username", username);
-				 //用户权限
-				 List<SPermission> spermlist=httpRequestUtils.httpPostReturnList(UriConstraints.API_PERMISSIONS ,map, SPermission.class);
-				 List<SPermission> permlist=SUserUtil.filterPersmList(spermlist);
-				 setSession(SUserUtil.SHIRO_CURRENT_USER_INFO,suser);
-				 setSession(SUserUtil.SHIRO_CURRENT_PERM_LIST_INFO,permlist);
-		} catch (Exception e) {
-			e.printStackTrace();
+		CasToken casToken = (CasToken)authcToken;
+		if(casToken==null){
+			return null;
 		}
-		return authc;
+		String ticket=(String)casToken.getCredentials();
+		log.info("ticket->"+ticket);
+		if(!StringUtils.hasText(ticket)){
+			return null;
+		}
+		TicketValidator ticketValidator = ensureTicketValidator();
+		try {
+        	// contact CAS server to validate service ticket
+        	Assertion casAssertion = ticketValidator.validate(ticket, getCasService());
+        	// get principal, user id and attributes
+        	AttributePrincipal casPrincipal = casAssertion.getPrincipal();
+        	String userId = casPrincipal.getName();
+ 
+        	Map<String, Object> attributes = casPrincipal.getAttributes();
+        	// refresh authentication token (user id + remember me)
+        	casToken.setUserId(userId);
+        	String rememberMeAttributeName = getRememberMeAttributeName();
+        	String rememberMeStringValue = (String)attributes.get(rememberMeAttributeName);
+        	boolean isRemembered = rememberMeStringValue != null && Boolean.parseBoolean(rememberMeStringValue);
+        	if (isRemembered) {
+            	casToken.setRememberMe(true);
+        	}
+        	// create simple authentication info
+        	List<Object> principals = CollectionUtils.asList(userId, attributes);
+        	PrincipalCollection principalCollection = new SimplePrincipalCollection(principals, getName());
+        	
+        	SUser suser=new SUser();
+			suser.setUsername(userId); 
+			suser.setToken((String)attributes.get("token"));
+			suser.setName(userId);
+	
+			HashMap  map=new HashMap();
+			map.put("username", userId);
+			//用户权限
+			List<SPermission> spermlist=httpRequestUtils.httpPostReturnList(UriConstraints.API_PERMISSIONS ,map, SPermission.class);
+			List<SPermission> permlist=SUserUtil.filterPersmList(spermlist);
+			setSession(SUserUtil.SHIRO_CURRENT_USER_INFO,suser);
+			setSession(SUserUtil.SHIRO_CURRENT_PERM_LIST_INFO,permlist);
+        	return new SimpleAuthenticationInfo(principalCollection, ticket);
+    	} catch (TicketValidationException e) { 
+        	throw new CasAuthenticationException("Unable to validate ticket [" + ticket + "]", e);
+    	} catch(AppException ex){
+    		throw new CasAuthenticationException(ex);
+    	}
 	}
 
 	/**
